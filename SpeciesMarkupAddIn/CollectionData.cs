c5aaf94b32ebaba535e67d84892b319ad1f7906a
Rudi von Staden <rudivs@gmail.com>
2014-11-23 22:29:26 +0200

    Added taxon class, text filter, months dictionary
     * Taxon class fields based on definition file supplied
     * Regex substitutions explained in code comments
    
    TODO:
    * Add fields for MinAlt, MaxAlt in TaxonPanel
    * Add methods to convert selected text to month

---
+++ SpeciesMarkupAddIn/CollectionData.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpeciesMarkupAddIn
+{
+    public static class CollectionData
+    {
+        public static readonly Dictionary<int, string> Months = new Dictionary<int, string>
+        {
+            { 1, "January" },
+            { 2, "February" },
+            { 3, "March" },
+            { 4, "April" },
+            { 5, "May" },
+            { 6, "June" },
+            { 7, "July" },
+            { 8, "August" },
+            { 9, "September" },
+            { 10, "October" },
+            { 11, "November" },
+            { 12, "December" }
+        };
+    }
+}
