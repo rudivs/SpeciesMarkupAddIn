1807f3cccebbd5e7a1713cf079a594ba01fa0b66
Rudi von Staden <rudivs@gmail.com>
2014-12-08 23:09:22 +0200

    Completed Excel export and various other updates
     * Cleaned up taxon getters and setters
     * Added required methods to TaxonList
     * Overhauled Excel export functionality
     * Tracking number set from file when taxon name details updated
     * Added ribbon tools to delete current record, clear batch, or copy species details from previous record
     * Added a dictionary and class to keep track of field names and settings

---
+++ SpeciesMarkupAddIn/Export.cs
@@ -0,0 +1,132 @@
+﻿using OfficeOpenXml;
+using OfficeOpenXml.Style;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SpeciesMarkupAddIn
+{
+    class Export
+    {
+        public static object GetPropValue(object src, string propName)
+        {
+            return src.GetType().GetProperty(propName).GetValue(src, null);
+        }
+
+        private static bool Validated()
+        {
+            if (Globals.ThisAddIn.currentBatch.Count < 1)
+                return false;
+            else
+            {
+                return true;
+            }
+        }
+
+        internal static void ExportToExcel()
+        {
+            if (Validated())
+            {
+                SaveFileDialog saveFileDialog = new SaveFileDialog();
+
+                saveFileDialog.Filter = "Excel files (*.xlsx)|*.xlsx|All files (*.*)|*.*";
+                saveFileDialog.FilterIndex = 1;
+                saveFileDialog.RestoreDirectory = false;
+
+                if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    ExportToExcelFile(saveFileDialog.FileName);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Exports current batch of marked up taxa to Excel.
+        /// </summary>
+        private static void ExportToExcelFile(string filename)
+        {
+            using (ExcelPackage p = new ExcelPackage())
+            {
+                //set the workbook properties and add a default sheet in it
+                SetWorkbookProperties(p);
+                //Create a sheet
+                ExcelWorksheet ws = CreateSheet(p, "Taxon Export");
+
+                // Add column headers
+                foreach (KeyValuePair<int, TaxonInternalReferences> entry in CollectionData.columnIndex)
+                {
+                    var cell = ws.Cells[1, entry.Key];
+                    cell.Value = entry.Value.FieldName;
+                    cell.Style.Fill.PatternType = ExcelFillStyle.Solid;
+                    cell.Style.Fill.BackgroundColor.SetColor(entry.Value.FillColor);
+                    cell.AddComment(entry.Value.Label, "Field description");
+                    ws.Column(entry.Key).Width = entry.Value.ColumnWidth;
+                }
+
+                int rowIndex = 2;
+                CreateData(ws, ref rowIndex);
+
+                //Generate file
+                try
+                {
+                    Byte[] bin = p.GetAsByteArray();
+                    File.WriteAllBytes(filename, bin);
+                }
+                catch (IOException ex)
+                {
+                    MessageBox.Show("Error writing Excel export file: " + ex.ToString(), "Export error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+                Globals.ThisAddIn.ResetCurrentBatch(CollectionData.DeleteAfterExport);
+            }
+        }
+
+        private static ExcelWorksheet CreateSheet(ExcelPackage p, string sheetName)
+        {
+            p.Workbook.Worksheets.Add(sheetName);
+            ExcelWorksheet ws = p.Workbook.Worksheets[1];
+            ws.Name = sheetName; //Setting Sheet's name
+            ws.Cells.Style.Font.Size = 11; //Default font size for whole sheet
+            ws.Cells.Style.Font.Name = "Calibri"; //Default Font name for whole sheet
+
+            return ws;
+        }
+
+        /// <summary>
+        /// Sets the workbook properties and adds a default sheet.
+        /// </summary>
+        private static void SetWorkbookProperties(ExcelPackage p)
+        {
+            //Here setting some document properties
+            p.Workbook.Properties.Title = "Taxon Markup Export";
+        }
+
+        private static void CreateData(ExcelWorksheet ws, ref int rowIndex)
+        {
+            for (int i = 0; i < Globals.ThisAddIn.currentBatch.Count; i++ )
+            {
+                Taxon t = Globals.ThisAddIn.currentBatch.GetByIndex(i);
+                for (int j = 1; j <= CollectionData.columnIndex.Keys.Max(); j++ )
+                {
+                    string prop = CollectionData.columnIndex[j].PropertyName;
+                    if (!string.IsNullOrEmpty(prop))
+                    {
+                        var cell = ws.Cells[rowIndex + i, j];
+                        cell.Value = GetPropValue(t, CollectionData.columnIndex[j].PropertyName);
+                        cell.Style.WrapText = true;
+                    }
+                }
+                ws.Row(rowIndex + i).Height = 50;
+            }
+        }
+    }
+}
--- SpeciesMarkupAddIn/Ribbon.cs
+++ SpeciesMarkupAddIn/Ribbon.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Microsoft.Office.Tools.Ribbon;
+using System.Windows.Forms;
 
 namespace SpeciesMarkupAddIn
 {
@@ -20,7 +21,35 @@ namespace SpeciesMarkupAddIn
 
         private void btnExportExcel_Click(object sender, RibbonControlEventArgs e)
         {
-            Globals.ThisAddIn.ExportToExcel();
+            Export.ExportToExcel();
+        }
+
+        private void btnDeleteCurrent_Click(object sender, RibbonControlEventArgs e)
+        {
+            DialogResult result = MessageBox.Show("Delete current record?", "Confirm delete", MessageBoxButtons.YesNo, MessageBoxIcon.Question, MessageBoxDefaultButton.Button1);
+            if (result == DialogResult.Yes)
+            {
+                Globals.ThisAddIn.currentBatch.DeleteCurrent();
+                Globals.ThisAddIn.myTaxonPanel.LoadCurrentTaxon();
+            }
+        }
+
+        private void btnClearBatch_Click(object sender, RibbonControlEventArgs e)
+        {
+            Globals.ThisAddIn.ResetCurrentBatch(CollectionData.DeleteAdHoc);
+        }
+
+        private void btnCopyPrevious_Click(object sender, RibbonControlEventArgs e)
+        {
+            int index = Globals.ThisAddIn.currentBatch.Index;
+            if (index > 0)
+            {
+                Taxon previousTaxon = Globals.ThisAddIn.currentBatch.GetByIndex(index-1);
+                Control taxonPanel = Globals.ThisAddIn.myTaxonPanel.Controls[1].Controls[0];
+                taxonPanel.Controls["textboxGenus"].Text = previousTaxon.Genus;
+                taxonPanel.Controls["textboxSpecies"].Text = previousTaxon.Species;
+                taxonPanel.Controls["textboxSpeciesAuthor"].Text = previousTaxon.SpeciesAuthor;
+            }
         }
     }
 }
